906793410df003da386ae8726b54add547bac3ac
Vladislav Kondratuk <vladislav.kondratuk@rambler.ru>
2020-05-19 23:51:10 +0300

    mockmvc test rest mapping PUT /activities

--- rest-app/src/test/java/com/demo/rest/api/controller/ActivityRestControllerTest.java
+++ rest-app/src/test/java/com/demo/rest/api/controller/ActivityRestControllerTest.java
@@ -2,7 +2,9 @@ package com.demo.rest.api.controller;
 
 import com.demo.rest.api.entity.Activity;
 import com.demo.rest.api.service.ActivityService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -33,6 +35,7 @@ class ActivityRestControllerTest {
     private static final int ONCE = 1;
     private static final long ACTIVITY_ID = 1L;
     private static final Activity ACTIVITY = new Activity();
+    private static final Activity UPDATED_ACTIVITY = new Activity();
     private static final List<Activity> ACTIVITIES = new ArrayList<>();
 
     @Mock
@@ -45,6 +48,13 @@ class ActivityRestControllerTest {
 
     private MockMvc mockMvc;
 
+    @BeforeAll
+    public static void initObjects() {
+        initActivityObject();
+        initUpdatedActivityObject();
+        ACTIVITIES.add(ACTIVITY);
+    }
+
     @BeforeEach
     public void setup() {
         initMocks(this);
@@ -52,9 +62,6 @@ class ActivityRestControllerTest {
                 .setMessageConverters(new MappingJackson2HttpMessageConverter())
                 .alwaysDo(MockMvcResultHandlers.print())
                 .build();
-
-        initActivityObject();
-        ACTIVITIES.add(ACTIVITY);
     }
 
     @Test
@@ -98,9 +105,19 @@ class ActivityRestControllerTest {
         verifyNoMoreInteractions(activityService);
     }
 
-    @Disabled
     @Test
-    void updateActivity() {
+    void shouldUpdateActivity() throws Exception {
+        doNothing().when(activityService).saveActivity(UPDATED_ACTIVITY);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put("/activities")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(UPDATED_ACTIVITY))
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+        ).andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(activityService, times(ONCE)).saveActivity(UPDATED_ACTIVITY);
+        verifyNoMoreInteractions(activityService);
     }
 
     @Disabled
@@ -108,13 +125,23 @@ class ActivityRestControllerTest {
     void deleteActivity() {
     }
 
-    private void initActivityObject() {
+    private static void initActivityObject() {
         ACTIVITY.setId(ACTIVITY_ID);
         ACTIVITY.setActivityName("test");
         ACTIVITY.setStatus("in progress");
-        ACTIVITY.setStartDate(Date.valueOf("2020-06-19"));
-        ACTIVITY.setPlannedEndDate(Date.valueOf("2020-06-19"));
-        ACTIVITY.setActualEndDate(Date.valueOf("2020-07-19"));
+        ACTIVITY.setStartDate(Date.valueOf("2020-05-17"));
+        ACTIVITY.setPlannedEndDate(Date.valueOf("2020-05-17"));
+        ACTIVITY.setActualEndDate(Date.valueOf("2020-05-18"));
         ACTIVITY.setComment("MockMvc test");
+    }
+
+    private static void initUpdatedActivityObject() {
+        UPDATED_ACTIVITY.setId(ACTIVITY_ID);
+        UPDATED_ACTIVITY.setActivityName("test");
+        UPDATED_ACTIVITY.setStatus("completed");
+        UPDATED_ACTIVITY.setStartDate(Date.valueOf("2020-05-17"));
+        UPDATED_ACTIVITY.setPlannedEndDate(Date.valueOf("2020-05-17"));
+        UPDATED_ACTIVITY.setActualEndDate(Date.valueOf("2020-05-18"));
+        UPDATED_ACTIVITY.setComment("MockMvc test");
     }
 }
