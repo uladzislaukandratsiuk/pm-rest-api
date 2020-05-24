8c8a7810b48bf11f4226a6376ecad99eee21dc5e
Vladislav Kondratuk <vladislavkondratuk322@gmail.com>
2020-05-24 20:25:57 +0300

    refactoring database schema

--- config/src/main/resources/sql/create-schema.sql
+++ config/src/main/resources/sql/create-schema.sql
@@ -6,53 +6,71 @@ use `project_management`;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
+
 DROP TABLE IF EXISTS `activity`;
 
 CREATE TABLE `activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `activity_name` varchar(128) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `planned_end_date` datetime DEFAULT NULL,
+  `activity_name` varchar(128) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `start_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `planned_end_date` datetime NOT NULL,
   `actual_end_date` datetime DEFAULT NULL,
   `comment` varchar(256) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `task_id` int(11) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+
+  KEY `FK_TASK_ID_idx` (`task_id`),
+
+  CONSTRAINT `FK_TASK`
+  FOREIGN KEY (`task_id`)
+  REFERENCES `task` (`id`)
+
+  ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 
 DROP TABLE IF EXISTS `task`;
 
 CREATE TABLE `task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `task_name` varchar(128) DEFAULT NULL,
-  `priority` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `planned_end_date` datetime DEFAULT NULL,
+  `task_name` varchar(128) NOT NULL,
+  `priority` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `start_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `planned_end_date` datetime NOT NULL,
   `actual_end_date` datetime DEFAULT NULL,
   `comment` varchar(256) DEFAULT NULL,
-  `activity_id` int(11) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+
   PRIMARY KEY (`id`),
-  KEY `FK_ACTIVITY_ID` (`activity_id`),
-  CONSTRAINT `FK_ACTIVITY` FOREIGN KEY (`activity_id`) 
-  REFERENCES `activity` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
+  KEY `FK_PROJECT_ID_idx` (`project_id`),
+
+  CONSTRAINT `FK_PROJECT`
+  FOREIGN KEY (`project_id`)
+  REFERENCES `project` (`id`)
+
+  ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 
 DROP TABLE IF EXISTS `project`;
 
 CREATE TABLE `project` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(128) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `start_date` datetime DEFAULT NULL,
-  `planned_end_date` datetime DEFAULT NULL,
+  `project_name` varchar(128) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  `start_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `planned_end_date` datetime NOT NULL,
   `actual_end_date` datetime DEFAULT NULL,
   `description` varchar(256) DEFAULT NULL,
-  `task_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_TASK_ID` (`task_id`),
-  CONSTRAINT `FK_TASK` FOREIGN KEY (`task_id`) 
-  REFERENCES `task` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
+  PRIMARY KEY (`id`)
+
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 
 SET FOREIGN_KEY_CHECKS = 1;
 
